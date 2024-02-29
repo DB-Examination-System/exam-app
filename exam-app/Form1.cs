32d0d350f90313cb58c745047f623ac33603f90b
mo3liii <mustafasoliman19@gmail.com>
2024-03-01 00:47:30 +0200

    start from mostafa

--- exam-app/Form1.cs
+++ exam-app/Form1.cs
@@ -1,10 +1,14 @@
+using exam_app.Models;
+
 namespace exam_app
 {
 	public partial class Form1 : Form
 	{
+		ItidbContext ItidbContext { get; set; } = new ItidbContext();
 		public Form1()
 		{
 			InitializeComponent();
+		
 		}
 	}
 }
