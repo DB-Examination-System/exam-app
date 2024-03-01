e107d67c65369b8745cf814939da866d0a8a4bd2
FatmaSorour8 <fatmamohamed862s@outlook.com>
2024-03-01 19:36:39 +0200

    create form for instructor-Crud.

---
+++ exam-app/CRUD_Instructor.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace exam_app
+{
+    partial class CRUD_Instructor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(211, 26);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(143, 29);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Enter Instructor ID";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(392, 19);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 40);
+            button1.TabIndex = 2;
+            button1.Text = "Show Data";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // CRUD_Instructor
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1018, 573);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "CRUD_Instructor";
+            Text = "CRUD_Student";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
