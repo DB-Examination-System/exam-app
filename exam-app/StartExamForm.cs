c012687053d39f1f25b5c6114e7a3b968278047b
Ahmed M samir <ahmed2017170572@cis.asu.edu.eg>
2024-03-03 20:31:12 +0200

    line changed

--- exam-app/StartExamForm.cs
+++ exam-app/StartExamForm.cs
@@ -17,7 +17,7 @@ namespace exam_app
     {
         // development part
         private int std_id = 1;
-        private int exam_id = 117; // 
+        private int exam_id = 117; 
         ItidbContext appContext = new ItidbContext();
         List<Question> examQuestionList; // questions of exam
         List<List<String>> questionChoices = new List<List<string>>();
@@ -271,15 +271,16 @@ namespace exam_app
 
         void submitAnswers()
         {
+          
             for (int i = 0; i < examQuestionList.Count; i++)
             {
-                
+               
                 appContext.ExamStdQuestions.Add(new ExamStdQuestion()
                 {
                     ExId = exam_id,
                     StId = std_id,
                     QId = examQuestionList[i].QId,
-                    StdAnswer = stdAns[i] ?? "no answer",
+                    StdAnswer = stdAns[i]== null ? "no answer": stdAns[i],
                 });
 
             }
