20c9ac53219eb3673acc4e81ff47e44c4545a63b
FatmaSorour8 <fatmamohamed862s@outlook.com>
2024-03-05 22:49:46 +0200

    Assign Course To Instructor is almost Done.

--- exam-app/AssignCoursesToInstructorForm.Designer.cs
+++ exam-app/AssignCoursesToInstructorForm.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AssignCoursesToInstructor";
+            cb_branch = new ComboBox();
+            lbl_branch = new Label();
+            lbl_course = new Label();
+            cb_track = new ComboBox();
+            lbl_insName = new Label();
+            btn_Assign_Course = new Button();
+            lbl_duration = new Label();
+            txt_duration = new TextBox();
+            cb_course = new ComboBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // cb_branch
+            // 
+            cb_branch.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            cb_branch.FormattingEnabled = true;
+            cb_branch.Location = new Point(263, 123);
+            cb_branch.Name = "cb_branch";
+            cb_branch.Size = new Size(363, 33);
+            cb_branch.TabIndex = 0;
+            // 
+            // lbl_branch
+            // 
+            lbl_branch.AutoSize = true;
+            lbl_branch.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            lbl_branch.Location = new Point(81, 123);
+            lbl_branch.Name = "lbl_branch";
+            lbl_branch.Size = new Size(74, 28);
+            lbl_branch.TabIndex = 1;
+            lbl_branch.Text = "Branch";
+            // 
+            // lbl_course
+            // 
+            lbl_course.AutoSize = true;
+            lbl_course.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            lbl_course.Location = new Point(81, 183);
+            lbl_course.Name = "lbl_course";
+            lbl_course.Size = new Size(135, 28);
+            lbl_course.TabIndex = 8;
+            lbl_course.Text = "Branch Tracks";
+            // 
+            // cb_track
+            // 
+            cb_track.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            cb_track.FormattingEnabled = true;
+            cb_track.Location = new Point(263, 183);
+            cb_track.Name = "cb_track";
+            cb_track.Size = new Size(363, 33);
+            cb_track.TabIndex = 7;
+            // 
+            // lbl_insName
+            // 
+            lbl_insName.AutoSize = true;
+            lbl_insName.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold);
+            lbl_insName.ForeColor = Color.MediumBlue;
+            lbl_insName.Location = new Point(143, 36);
+            lbl_insName.Name = "lbl_insName";
+            lbl_insName.Size = new Size(423, 40);
+            lbl_insName.TabIndex = 10;
+            lbl_insName.Text = "Instructor Name: Fatma Sorour";
+            // 
+            // btn_Assign_Course
+            // 
+            btn_Assign_Course.BackColor = SystemColors.ActiveCaption;
+            btn_Assign_Course.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            btn_Assign_Course.ForeColor = SystemColors.ButtonFace;
+            btn_Assign_Course.Location = new Point(263, 396);
+            btn_Assign_Course.Name = "btn_Assign_Course";
+            btn_Assign_Course.Size = new Size(169, 62);
+            btn_Assign_Course.TabIndex = 11;
+            btn_Assign_Course.Text = "Assign Course";
+            btn_Assign_Course.UseVisualStyleBackColor = false;
+            btn_Assign_Course.Click += btn_Assign_Course_Click;
+            // 
+            // lbl_duration
+            // 
+            lbl_duration.AutoSize = true;
+            lbl_duration.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            lbl_duration.Location = new Point(81, 293);
+            lbl_duration.Name = "lbl_duration";
+            lbl_duration.Size = new Size(160, 28);
+            lbl_duration.TabIndex = 12;
+            lbl_duration.Text = "Course Duration";
+            // 
+            // txt_duration
+            // 
+            txt_duration.Font = new Font("Segoe UI Semibold", 10.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txt_duration.Location = new Point(263, 293);
+            txt_duration.Name = "txt_duration";
+            txt_duration.Size = new Size(363, 31);
+            txt_duration.TabIndex = 13;
+            // 
+            // cb_course
+            // 
+            cb_course.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            cb_course.FormattingEnabled = true;
+            cb_course.Location = new Point(263, 240);
+            cb_course.Name = "cb_course";
+            cb_course.Size = new Size(363, 33);
+            cb_course.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.8125F, FontStyle.Bold);
+            label1.Location = new Point(81, 240);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 28);
+            label1.TabIndex = 6;
+            label1.Text = "Branch Courses";
+            // 
+            // AssignCoursesToInstructor
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(710, 506);
+            Controls.Add(txt_duration);
+            Controls.Add(lbl_duration);
+            Controls.Add(btn_Assign_Course);
+            Controls.Add(lbl_insName);
+            Controls.Add(lbl_course);
+            Controls.Add(cb_track);
+            Controls.Add(label1);
+            Controls.Add(cb_course);
+            Controls.Add(lbl_branch);
+            Controls.Add(cb_branch);
+            Name = "AssignCoursesToInstructor";
+            Text = "AssignCoursesToInstructor";
+            Load += AssignCoursesToInstructor_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox cb_branch;
+        private Label lbl_branch;
+        private Label lbl_course;
+        private ComboBox cb_track;
+        private Label lbl_insName;
+        private Button btn_Assign_Course;
+        private Label lbl_duration;
+        private TextBox txt_duration;
+        private ComboBox cb_course;
+        private Label label1;
     }
 }
